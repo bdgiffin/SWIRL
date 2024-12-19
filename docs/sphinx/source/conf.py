ee155311ddade942c87eeaf39cdcb47b1842653e
Brian Giffin <bdgiffin@brians-mbp.eduroam-corewlcs-14.okstate.edu>
2024-12-18 18:41:42 -0600

    Cleaned up conf.py

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -23,8 +23,6 @@ bibtex_default_style = 'unsrt'
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
